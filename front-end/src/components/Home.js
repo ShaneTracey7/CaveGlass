abc988d7352562a7e88efcfd9b7e47dafd068a9d
Shane Tracey <71601674+ShaneTracey7@users.noreply.github.com>
2025-04-30 21:33:11 -0400

    fix

--- front-end/src/components/Home.js
+++ front-end/src/components/Home.js
@@ -36,8 +36,8 @@ function Main(props)
           switch(type)
           {
             case "ok": console.log("gameArrRef.current[0]: " + JSON.stringify(gameArrRef.current[0]));props.setgame(gameArrRef.current[0]); props.setingame(true); console.log("case 'ok' "); break;
-            case "up": if(gameIndexRef.current > 0){setGameIndex(gameIndexRef.current + 1)}; console.log("case 'up' "); console.log(gameIndexRef.current + " > 0");break;
-            case "down": if(gameIndexRef.current < (gameArrRef.current.length - 1)){setGameIndex(gameIndexRef.current + 1)}; console.log(gameIndexRef.current + " < " + (gameArrRef.current.length - 1));console.log("case 'down' "); break;
+            case "up": if(gameIndexRef.current > 0){setGameIndex((gameIndexRef.current - 1))}; console.log("case 'up' "); console.log(gameIndexRef.current + " > 0");break;
+            case "down": if(gameIndexRef.current < (gameArrRef.current.length - 1)){setGameIndex((gameIndexRef.current + 1))}; console.log(gameIndexRef.current + " < " + (gameArrRef.current.length - 1));console.log("case 'down' "); break;
             default: console.log("wrong type");break;
           }
         });
