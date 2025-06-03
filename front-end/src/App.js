016643534e16bd7569ccc7e6e3c909de0c075f7d
Shane Tracey <71601674+ShaneTracey7@users.noreply.github.com>
2025-06-03 14:12:11 -0400

    got rid of db

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -8,120 +8,13 @@ import { isMobile } from 'react-device-detect';
 import './styles.css';
 import axios from 'axios';
 import Mobile from './components/Mobile';
-import { io } from 'socket.io-client';
-import socket from './components/Socket';
 
 function App() {
   
   const [inGame, setInGame] = useState(false); //if true score reaction is displayed,
   const [game, setGame] = useState([]); 
-  const [mobileKey, setMobileKey] = useState(""); 
-  const mobileKeyRef = useRef(0);
-  const [enteredKey, setEnteredKey] = useState(0); 
-  const [mobileConnection, setMobileConnection] = useState(false); //maybe should be ref
-
-  const [isRC, setIsRC] = useState(false); 
-  const isRCRef = useRef(false); 
-
-   useEffect(() => {    
-        isRCRef.current = isRC;    
-          
-            }, [isRC]);
 
   let backendUrl = 'https://caveglass.onrender.com';//'http://localhost:8080';// https://caveglass.onrender.com
-  
-
-   //emit socket stuff inside of home/game (can't access useStates from app.js(here))
-   useEffect(() => { 
-      //get key from backend
-      if(!isMobile)
-      {
-        apiGetKey();
-        
-        
-        socket.emit('ping');
-        socket.on('pong', () => {
-        console.log('Received pong');
-    });
-        /*
-        socket.on('remoteMove', (msg) => {
-          //setMessage(msg);
-        });*/
-      }
-      else //is a Mobile device
-      {
-
-      }
-
-      
-
-      const handleUnload = () => {
-        const data = { type: 'removeKey', key: mobileKeyRef.current };
-        const blob = new Blob([JSON.stringify(data)], { type: 'application/json' });
-    
-        navigator.sendBeacon(backendUrl + '/key', blob);
-        console.log("unloading");
-        console.log("mobilekey: " + mobileKey);
-      };
-    
-      
-
-      window.addEventListener('unload', handleUnload);
-    
-      return () => {
-        socket.off('pong');
-        socket.disconnect();
-        window.removeEventListener('unload', handleUnload);
-      };
-      }, []);
-
-      useEffect(() => {
-        if(!isMobile)
-          {
-            mobileKeyRef.current = mobileKey;
-            if (mobileKeyRef.current != 0 && mobileKeyRef.current != "")
-            {
-              socket.emit('register', { code: mobileKey}/*mobileKeyRef.current*/); //new
-            }
-            
-          }
-      }, [mobileKey]);
-
-  const apiGetKey = () => {
-    axios.post(backendUrl + '/key', {
-        type: 'setKey',
-      }, {
-        headers: {
-        'content-type': 'application/json'
-        }}).then((data) => {
-          console.log(data);
-          
-          
-          if(!isNaN(data.data.key)) //NEEDS TO BE A NUMBER
-          {
-            setMobileKey(data.data.key);
-            setMobileConnection(false);
-          }
-          else{
-            console.log("not a number");
-          }
-   })
-   console.log("api get key was called");
-  }
-/*
-  const apiRemoveKey = () => {
-    axios.post(backendUrl + '/key', {
-        type: 'removeKey',
-        key: mobileKey,
-      }, {
-        headers: {
-        'content-type': 'application/json'
-        }}).then((data) => {
-          console.log(data.data.message);
-        })
-   console.log("api remove key was called");
-  }
-*/
 
   let display;
   /*
@@ -152,24 +45,23 @@ function App() {
  
     if (isMobile)
       {
-        display = <Mobile socket={socket} />;
+        display = <Mobile />;
       }
       else
       {
       
-        if( inGame)
+        if(inGame)
           {
-            display = <Game setIsRC={setIsRC} isRC={isRC} isRCRef={isRCRef.current} game={game} socket={socket} setgame={setGame} setingame={setInGame}/>;
+            display = <Game game={game} setgame={setGame} setingame={setInGame}/>;
           }
           else
           {
-            display = <Home setIsRC={setIsRC} isRC={isRC} isRCRef={isRCRef.current} mobileKey={mobileKey} socket={socket} setgame={setGame} setingame={setInGame}/>;
+            display = <Home setgame={setGame} setingame={setInGame}/>;
           }
       }
           
     return (
       <div className="App">
-      {/*mobileKey /* only here for testing */}
       {display}
       
       </div>
