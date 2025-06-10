0a80b2b4e8798e6044380a5800bf9aebda638c40
Shane Tracey <71601674+ShaneTracey7@users.noreply.github.com>
2025-06-10 15:35:08 -0400

    Update index.js

--- index.js
+++ index.js
@@ -254,7 +254,7 @@ app.post("/", (req, res) => {
 
 const PORT = process.env.PORT || 8080;
 
-app.listen(PORT, '0.0.0.0', () => {
+app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
 
